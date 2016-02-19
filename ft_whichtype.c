35a0063fefc6d4feb51fa6338041b2cf6760552b
Anthony LAULOM <alaulom@e3r6p16.42.fr>
2016-02-19 18:09:03 +0100

    gg

--- ft_whichtype.c
+++ ft_whichtype.c
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_whichtype.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/19 15:28:28 by alaulom           #+#    #+#             */
-/*   Updated: 2016/02/19 18:08:05 by alaulom          ###   ########.fr       */
+/*   Created: 2016/02/19 17:46:53 by alaulom           #+#    #+#             */
+/*   Updated: 2016/02/19 17:54:23 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-	#define FT_PRINTF_H
+#include "ft_printf.h"
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-
-int		ft_printf(const char* format, ...);
-int		ft_whichtype(const char*s, int i, va_list ap);
-int		istype_c(va_list ap);
-void	ft_putchar(char c);
-void	ft_putstr(const char* s);
-void	ft_putnbr(int nb);
-size_t	ft_strlen(const char* s);
-
-#endif
+int			ft_whichtype(const char* s, int i, va_list ap)
+{
+	if (s[i] == 'c')
+		return (istype_c(ap));
+	if (s[i] == '%')
+	{
+		ft_putchar('%');
+		return (1);
+	}
+}
