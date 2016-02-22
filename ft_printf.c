537f975790d5549056e367581a277065181e4ffa
Anthony LAULOM <alaulom@e3r6p15.42.fr>
2016-02-22 18:36:11 +0100

    main

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/20 18:26:22 by alaulom           #+#    #+#             */
-/*   Updated: 2016/02/20 18:58:58 by alaulom          ###   ########.fr       */
+/*   Updated: 2016/02/22 18:35:41 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int			ft_printf(const char* format, ...)
 {
 	va_list	ap;
+	int		i;
 	int		cmp;
 	char*	str;
 
+	i = 0;
 	cmp = 0;
 	va_start(ap, format);
 	str = (char*)malloc(sizeof(char) * (ft_strlen(format) + 1));
@@ -26,6 +28,13 @@ int			ft_printf(const char* format, ...)
 	{
 		if (*str != '%')
 		{
+			ft_putchar(str[i++]);
+			cmp++;
 		}
+		else if (*str == '%')
+			cmp += ft_whichtype(str, i++, ap);
+		i++;
 	}
+	va_end(va_list ap);
+	return (cmp);
 }
