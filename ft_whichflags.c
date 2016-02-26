c2a4a037168b66ca22ae1ce9d0df6a0f7828a006
Anthony LAULOM <alaulom@e3r4p18.42.fr>
2016-02-26 14:35:10 +0100

    refonte

--- ft_whichflags.c
+++ ft_whichflags.c
@@ -1,40 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_whichflags.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/20 18:26:22 by alaulom           #+#    #+#             */
-/*   Updated: 2016/02/26 14:34:42 by alaulom          ###   ########.fr       */
+/*   Created: 2016/02/26 13:15:30 by alaulom           #+#    #+#             */
+/*   Updated: 2016/02/26 14:34:29 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-int				ft_printf(const char *format, ...)
+int			ft_whichflags(char *format)
 {
-	va_list		ap;
-	int			ret;
-	int			i;
+	int		i;
 
-	va_start(ap, format);
-	ret = 0;
 	i = 0;
 	while (format[i])
 	{
-		if (format[i] != '%')
-		{
-			ft_putchar(format[i++]);
-			ret++;
-		}
-		else if (format[i] == '%')
-		{
-			ret += ft_whichflags((char*)(format[i + 1]));
-			ret += ft_whichtype((char*)(format[i + 1]));
-		}
+		if (format[i] == '#' || format[i] == '-' || format[i] == '+' ||
+			   	format[i] == ' ')
+			i += ft_options(format[i]);
+		else if (ft_isdigit(format[i]) && format[i - 1] != '.')
+			i += ft_width(&format[i]);
+		else if (format[i] == '.')
+			i += ft_precision(&format[i]);
+		else if (format[i] == 'h' || format[i] == 'l' || format[i] == 'j'
+				|| format[i] == 'z')
+			i += ft_size_options(&format[i]);
 		i++;
 	}
-	va_end(ap);
-	return (ret);
+	return (i);
 }
--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 15:28:28 by alaulom           #+#    #+#             */
-/*   Updated: 2016/02/24 17:44:34 by alaulom          ###   ########.fr       */
+/*   Updated: 2016/02/26 14:34:40 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-typedef struct			s_env
-{
-	int					min;
-	int					zero;
-	int					sharp;
-	int					plus;
-	int					space;
-	int					size;
-	int					isprecision;
-	int					precision;
-	int					per_cent;
-	char		params;
-	int					result;
-	int					i;
-}						t_env;
-
-typedef struct			s_tab
-{
-	int					(*tab_ft_conv[126])(va_list ap, t_env *env);
-}						t_tab;
-
 int		ft_printf(const char *format, ...);
-int		ft_whichtype(va_list ap, char c);
+int		ft_whichtype(char *format);
+int		ft_wichflags(char *format);
 int		istype_c(va_list ap);
 int		istype_d(va_list ap);
 int		istype_s(va_list ap);
