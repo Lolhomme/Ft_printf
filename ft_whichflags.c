fb08b5aa2577a5e27db1427cba317b72071f963d
Anthony LAULOM <alaulom@e3r4p18.42.fr>
2016-02-26 17:17:22 +0100

    inprogress

--- ft_whichflags.c
+++ ft_whichflags.c
@@ -6,13 +6,13 @@
 /*   By: alaulom <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 13:15:30 by alaulom           #+#    #+#             */
-/*   Updated: 2016/02/26 16:49:06 by alaulom          ###   ########.fr       */
+/*   Updated: 2016/02/26 17:16:47 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-int			ft_whichflags(char *format)
+int			ft_whichflags(char *format, t_env *env)
 {
 	int		i;
 
@@ -21,14 +21,19 @@ int			ft_whichflags(char *format)
 	{
 		if (format[i] == '#' || format[i] == '-' || format[i] == '+' ||
 				format[i] == ' ')
-			i += ft_options(format[i]);
-		else if (ft_isdigit(format[i]) && format[i - 1] != '.')
-			i += ft_width(&format[i]);
+			ft_options(format[i], env);
+		else if (ft_isdigit(format[i]) && env->isprecision == 0)
+			i =+ ft_width(&format[i], env);
 		else if (format[i] == '.')
-			i += ft_precision(&format[i]);
+			i += ft_precision(&format[i], env);
 		else if (format[i] == 'h' || format[i] == 'l' || format[i] == 'j'
 				|| format[i] == 'z')
-			i += ft_size_options(&format[i]);
+			i += ft_size_options(&format[i], env);
+		else
+		{
+			env->params = format[i];
+			return (i);
+		}
 		i++;
 	}
 	return (i);
